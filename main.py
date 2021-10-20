8ce75f5e4867a634b76365ad1639b9b71f26b536
VZYKIN <92172085+VZYKIN@users.noreply.github.com>
2021-10-20 22:25:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-import math 
+from decimal import *
+from math import factorial
 
-def CalculatePi(roundVal):
+def chudnovsky(n):
+pi = Decimal(0)
+k = 0
+while k < n:
+pi += (Decimal(-1)**k) * (Decimal(factorial(6 * k)) / ((factorial(k)**3) * (factorial(3 * k))) * (13591409 + 545140134 * k) / (640320**(3 * k)))
+k += 1
+pi = pi * Decimal(10005).sqrt() / 4270934400
+pi = pi**(-1)
+return pi
 
-		somepi = round(math.pi,roundVal);
-		pi = str(somepi)
-		someList = list(pi)
-		return somepi;
-	
-	
 
+def chudnovsky2(n):
+pi = Decimal(13591409)
+ak = Decimal(1)
+k = 1
+while k < n:
+ak *= -Decimal((6*k-5)*(2*k-1)*(6*k-1))/Decimal(k*k*k*26680*640320*640320)
 
+val = ak*(13591409 + 545140134*k)
 
-roundTo = raw_input('Enter the number of digits you want after the decimal for Pi: ')
-try:
-	roundint = int(roundTo);
-	print CalculatePi(roundint);
-except:
-	print "You did not enter an integer";
+d = Decimal((6*k-5)*(2*k-1)*(6*k-1))/Decimal(k*k*k*26680*640320*640320)
+
+pi += val
+k += 1
+pi = pi * Decimal(10005).sqrt()/4270934400
+pi = pi**(-1)
+return pi
+
+n = 70
+getcontext().prec = n
+print(chudnovsky(n))
+print(chudnovsky2(n))
